7eeac189252943ca00cc8c3ec5830007a0ca112d
Vinay Kumar <vinaydagar514@gmail.com>
2025-02-16 22:45:14 +0530

    Added Hero image animation

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className}`}>
           {/* Header */}
           <Header />
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="mt-40">
-      <Button>Vinay Dagar</Button>
+      <HeroSection />
     </div>
   );
 }

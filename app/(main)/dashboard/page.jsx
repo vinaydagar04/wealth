873824b78ffc22bd0041ab9e6c17a9a394f16b7f
Vinay Kumar <vinaydagar514@gmail.com>
2025-02-19 22:00:50 +0530

    CreateAccount section in api

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default async function DashboardPage() {
+  return (
+    <div className="px-5">
+      {/* Budget Progress */}
+
+      {/* Overview */}
+
+      {/* Accounts Grid  */}
+
+      <div>
+        <CrateAccountDrawer>
+            
+        </CrateAccountDrawer>
+      </div>
+    </div>
+  );
+}
---
+++ components/create.account.drawer.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const CrateAccountDrawer = () => {
+  return <div>CreateAccountDrawer</div>;
+};
+
+export default CrateAccountDrawer;

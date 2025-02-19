c8d0749352baa314c7c8181f2ece0a19dbc0cd4f
Vinay Kumar <vinaydagar514@gmail.com>
2025-02-19 15:39:30 +0530

    stored user in database

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
-
-const Header = () => {
+import { checkUser } from "@/lib/checkUser";
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
